ff9b6686c26075906d9b45d88c9aacc0d9e94eff
Kai Wang <charles.wangkai@gmail.com>
2023-08-30 20:31:42 +0800

    Add judge0 attrs and custom judge for problem tetris

---
+++ kickstart/2014/round_c/tetris/output_validators/CustomJudge.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class CustomJudge {
+  static final double EPSILON = 1e-6;
+
+  public static void main(String[] args) throws Throwable {
+    judge(args[0], args[1], args[2]);
+  }
+
+  static void judge(String inputFile, String outputFile, String answerFile) throws Throwable {
+    Scanner outputSc = new Scanner(new File(outputFile));
+    Scanner answerSc = new Scanner(new File(answerFile));
+
+    while (true) {
+      if (outputSc.hasNext() != answerSc.hasNext()) {
+        throw new RuntimeException();
+      }
+      if (!outputSc.hasNext()) {
+        break;
+      }
+
+      String outputToken = outputSc.next();
+      String answerToken = answerSc.next();
+      if (!outputToken.equals(answerToken)) {
+        throw new RuntimeException();
+      }
+    }
+
+    answerSc.close();
+    outputSc.close();
+  }
+}
